f7a5e96d923918e9fc8a8f3d321fa13658dacca1
JaredIannarino <Iannarinoj@gmail.com>
2023-03-25 16:55:59 -0400

    Added the chrome extension
    did some media query stuff too. Added a main.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
-const logoTop = document.getElementById("logo-top")
 
-window.addEventListener("resize", function(){
-    console.log(window.screen.width)
-    if(window.screen.width > 599){
-        logoTop.innerHTML = `
-        <img src="img/devJared.png" alt="A logo that says dev id='Jared'">
-        `
-    }else{
-        logoTop.innerHTML = ``
-    }
-})
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
